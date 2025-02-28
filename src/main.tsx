8eac7e4928ba89fff23fd1a39181f0a84884e080
vaibhav <vaibhavva219@gmail.com>
2025-02-28 19:28:55 +0530

    Pixel

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ declare global {
   interface Window {
     dataLayer: any[];
     gtag: (...args: any[]) => void;
+    twq: (...args: any[]) => void;
   }
 }
 
@@ -29,7 +30,21 @@ const addGoogleAds = () => {
   document.head.appendChild(configScript);
 };
 
+// Add Twitter pixel tracking
+const addTwitterPixel = () => {
+  // Add Twitter conversion tracking base code
+  const twitterScript = document.createElement('script');
+  twitterScript.innerHTML = `
+    !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+    },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+    a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+    twq('config','p791z');
+  `;
+  document.head.appendChild(twitterScript);
+};
+
 // Initialize tracking when the app starts
 addGoogleAds();
+addTwitterPixel();
 
 createRoot(document.getElementById("root")!).render(<App />);
